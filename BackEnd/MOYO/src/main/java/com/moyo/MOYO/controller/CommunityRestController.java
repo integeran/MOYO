66f23e75e6c50a056ce8dcc58d1eed3811c3b329
김성찬 <tjdcksdl00@naver.com>
2020-01-21 18:31:01 +0900

    DM firebase connect and CRUD

--- BackEnd/MOYO/src/main/java/com/moyo/MOYO/controller/CommunityRestController.java
+++ BackEnd/MOYO/src/main/java/com/moyo/MOYO/controller/CommunityRestController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@CrossOrigin("*")
 public class CommunityRestController {
 	
 	@Autowired
@@ -28,6 +30,38 @@ public class CommunityRestController {
 		} catch(RuntimeException e) {
 			return response(e.getMessage(), HttpStatus.CONFLICT, false);
 		}
+	}
+	
+	@GetMapping("/DM/testID")
+	public ResponseEntity<Map<String, Object>> testID() {
+		try {
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+
+			resultMap.put("sender", tempObject(5, "INDONG", 28, "m", null, 1, 2, "kunder@kakao.com", 0, 1, "2020-01-20 06:28:47", "2020-01-20 06:28:47"));
+			resultMap.put("receiver", tempObject(6, "SUNGCHAN", 25, "m", null, 2, 5, "tjdcksdl00@kakao.com", 0, 2, "2020-01-22 16:28:47", "2020-01-23 19:21:43"));
+			return response(resultMap, HttpStatus.OK, true);
+		} catch(RuntimeException e) {
+			return response(e.getMessage(), HttpStatus.CONFLICT, false);
+		}
+	}
+	
+	private Map<String, Object> tempObject(int uId, String nickname, int age, String gender, String image, int beforePsId, 
+			int afterPsId, String socialId, int provider, int level, String registerDate, String updateDate) {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("uId", uId);
+		resultMap.put("nickname", nickname);
+		resultMap.put("age", age);
+		resultMap.put("gender", gender);
+		resultMap.put("image", image);
+		resultMap.put("beforePsId", beforePsId);
+		resultMap.put("afterPsId", afterPsId);
+		resultMap.put("socialId", socialId);
+		resultMap.put("provider", provider);
+		resultMap.put("level", level);
+		resultMap.put("registerDate", registerDate);
+		resultMap.put("updateDate", updateDate);
+		
+		return resultMap;
 	}
 	
 	private ResponseEntity<Map<String, Object>> response(Object data, HttpStatus httpstatus, boolean status) {
