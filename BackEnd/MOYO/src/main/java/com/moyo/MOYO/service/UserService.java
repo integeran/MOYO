f00b73cde8126d40b965fa2240c4dcb52734fc86
IndongLee <kundera@kakao.com>
2020-01-22 18:01:05 +0900

    update JWT feature

--- BackEnd/MOYO/src/main/java/com/moyo/MOYO/service/UserService.java
+++ BackEnd/MOYO/src/main/java/com/moyo/MOYO/service/UserService.java
@@ -7,14 +7,16 @@ import com.moyo.MOYO.dto.User;
 public interface UserService {
 	public List<User> selectAll();
 	
+	public User selectOne(int uId);
+	
+	public User selectOneBySocialId(String socialId, int provider);
+	
+	public User selectOneByNickname(String nickname);
+	
 	public int register(User user);
 	
 	public int delete(int uId);
 	
 	public int update(User user);
-	
-	public User selectOne(int uId);
-	
-	public User selectOneBySocialId(String socialId, int provider);
 
 }
