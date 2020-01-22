aee3bd412ed9ba55127fa3a9029721a8bf613834
integeran <jmiha22@gmail.com>
2020-01-22 18:27:04 +0900

    update accompany mapper

--- BackEnd/MOYO/src/main/java/com/moyo/MOYO/dto/AccompanyBoard.java
+++ BackEnd/MOYO/src/main/java/com/moyo/MOYO/dto/AccompanyBoard.java
@@ -22,4 +22,9 @@ public class AccompanyBoard {
 	private String deadlineToggle;
 	private String registerDate;
 	private String updateDate;
+	
+	private String nickname;
+	private String nation;
+	private String city;
+	private String type;
 }
