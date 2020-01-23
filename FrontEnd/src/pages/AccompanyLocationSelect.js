7d187e16962fd4f59a5e9b1565f5df618afa10ba
Youngjune Jang <cyj9212@gmail.com>
2020-01-23 13:15:03 +0900

    add accompany features

--- FrontEnd/src/pages/AccompanyLocationSelect.js
+++ FrontEnd/src/pages/AccompanyLocationSelect.js
@@ -1,24 +1,87 @@
-import React, { useState, useCallback } from 'react';
-import ListNation from '../components/accompany/ListNation';
-import ListCity from '../components/accompany/ListCity';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { useDispatch } from 'react-redux';
+import { accompanyNation, accompanyCity } from '../modules/accompanyCondition';
 
-const ListContainer = styled.div``;
+const nationData = [
+  { nid: 2, name: '프랑스' },
+  { nid: 3, name: '스페인' },
+  { nid: 4, name: '이탈리아' },
+  { nid: 5, name: '스위스' },
+  { nid: 6, name: '영국' },
+  { nid: 7, name: '독일' },
+  { nid: 8, name: '포르투갈' },
+  { nid: 9, name: '크로아티아' },
+];
+const cityData = [
+  { cid: 1, nid: 2, name: '파리' },
+  { cid: 2, nid: 2, name: '니스' },
+  { cid: 3, nid: 3, name: '바르셀로나' },
+  { cid: 4, nid: 3, name: '마드리드' },
+  { cid: 5, nid: 4, name: '피렌체' },
+  { cid: 6, nid: 4, name: '로마' },
+];
 
-const AccompanyLonationSelect = () => {
-  const [nation, setNation] = useState('');
-  const [city, setCity] = useState('');
+const Title = styled.h2`
+  margin: 0;
+  padding: 1rem 1rem 2rem 1rem;
+`;
 
-  const onSelectNation = useCallback(nation => setNation(nation), []);
-  const onSelectCity = useCallback(city => setNation(city), []);
+const ListContainer = styled.div`
+  display: flexbox;
+`;
+
+const NationList = styled(List)`
+  flex-grow: 1;
+`;
+
+const CityList = styled(List)`
+  flex-grow: 3;
+`;
+
+const AccompanyLonationSelect = ({ history }) => {
+  const [cityList, setCityList] = useState([]);
+  const dispatch = useDispatch();
+
+  const onNationClick = nationItem => {
+    setCityList(cityData.filter(item => item.nid === nationItem.nid));
+    dispatch(accompanyNation({ code: nationItem.nid, name: nationItem.name }));
+  };
+
+  const onCityClick = cityItem => {
+    dispatch(accompanyCity({ code: cityItem.cid, name: cityItem.name }));
+    history.push('/acc/accSetDate');
+  };
+
   return (
-    <>
-      <h3>장소를 선택해주세요.</h3>
+    <div>
+      <Title>어디로 가실건가요?</Title>
       <ListContainer>
-        <ListNation onSelectNation={onSelectNation} />
-        <ListCity nation={nation} onSelectCity={onSelectCity} />
+        <NationList component="nav">
+          {nationData.map(item => (
+            <ListItem
+              alignItems="center"
+              button
+              key={item.nid}
+              onClick={() => onNationClick(item)}
+            >
+              <ListItemText primary={item.name} />
+            </ListItem>
+          ))}
+        </NationList>
+        <CityList component="nav">
+          {cityList.size !== 0 &&
+            cityList.map(item => (
+              <ListItem button key={item.cid} onClick={() => onCityClick(item)}>
+                <ListItemText primary={item.name} />
+              </ListItem>
+            ))}
+        </CityList>
       </ListContainer>
-    </>
+    </div>
   );
 };
 
--- FrontEnd/src/pages/AccompanyMain.js
+++ FrontEnd/src/pages/AccompanyMain.js
@@ -3,31 +3,32 @@ import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 import Header from '../components/common/Header';
 
-const AccompanyMain = () => {
-  const MainButtonDiv = styled.div`
-    display: flex;
-    justify-content: space-around;
-    align-content: center;
-  `;
+const MainButtonDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+`;
 
-  const MainButton = styled(Button)`
-    padding: 3rem;
-    border: 1px solid black;
-  `;
+const MainButton = styled(Button)`
+  padding: 3rem;
+  border: 1px solid black;
+`;
+
+const AccompanyMain = ({ history }) => {
+  const handleAccompanyFindClick = () => {
+    history.push('/acc/accSetLoc');
+  };
   return (
     <div>
       <Header leftSet="뒤로" midTitle="메인" rightSet="채팅" />
       <p>오늘은 여행이 어때요?</p>
       <MainButtonDiv>
-        <MainButton variant="outlined">동행찾기</MainButton>
-        <MainButton variant="outlined">동행모으기</MainButton>
+        <MainButton variant="outlined" onClick={handleAccompanyFindClick}>
+          동행 구하기
+        </MainButton>
+        <MainButton variant="outlined">동행 글 작성</MainButton>
       </MainButtonDiv>
       <p>일정</p>
-      <p>아아</p>
-      <p>아아</p>
-      <p>아아</p>
-      <p>아아</p>
-      <p>아아</p>
     </div>
   );
 };
