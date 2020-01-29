e5fb610f4e8b8bb2b02f622ec0041ea9585bf75e
IndongLee <kundera@kakao.com>
2020-01-29 17:11:59 +0900

    update CORS

--- BackEnd/MOYO/src/main/java/com/moyo/MOYO/interceptor/WebConfig.java
+++ BackEnd/MOYO/src/main/java/com/moyo/MOYO/interceptor/WebConfig.java
@@ -3,29 +3,36 @@ package com.moyo.MOYO.interceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	private static final String[] EXCLUDE_PATHS = {
-			"user/**",
-			"swagger-ui.html#/**"
+			"/user/**",
+			"/v2/api-docs",
+			"/swagger-resources/**",
+			"/swagger-ui.html/**",
+			"/webjars/**"
 	};
 	
 	@Autowired
 	private JwtInterceptor jwtInterceptor;
 	
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(jwtInterceptor)
-//										.addPathPatterns("/**")
-//										.excludePathPatterns(EXCLUDE_PATHS);
-//	}
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(jwtInterceptor)
+										.addPathPatterns("/**")
+										.excludePathPatterns(EXCLUDE_PATHS);
+	}
 	
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        		.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD");
+        		.allowedOrigins("*")
+        		.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
+        		.allowedHeaders("*")
+        		.maxAge(3600);
     }
 	
 }
