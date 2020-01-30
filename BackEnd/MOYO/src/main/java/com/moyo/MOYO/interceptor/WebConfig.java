37aa85c03b20345769c5754d1e7a75c34ae125ac
김성찬 <tjdcksdl00@naver.com>
2020-01-30 09:17:17 +0900

    update

--- BackEnd/MOYO/src/main/java/com/moyo/MOYO/interceptor/WebConfig.java
+++ BackEnd/MOYO/src/main/java/com/moyo/MOYO/interceptor/WebConfig.java
@@ -2,6 +2,7 @@ package com.moyo.MOYO.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
