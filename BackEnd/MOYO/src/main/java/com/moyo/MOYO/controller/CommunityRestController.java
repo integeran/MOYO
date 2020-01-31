20a10cea8404e48a68ea87d4644a1626fbd8853e
김성찬 <tjdcksdl00@naver.com>
2020-01-31 10:59:01 +0900

    test

--- BackEnd/MOYO/src/main/java/com/moyo/MOYO/controller/CommunityRestController.java
+++ BackEnd/MOYO/src/main/java/com/moyo/MOYO/controller/CommunityRestController.java
@@ -28,38 +28,6 @@ public class CommunityRestController {
 	@Autowired
 	CommunityService cService;
 	
-	@GetMapping("/DM/testID")
-	public ResponseEntity<Map<String, Object>> testID() {
-		try {
-			Map<String, Object> resultMap = new HashMap<String, Object>();
-
-			resultMap.put("receiver", tempObject(5, "INDONG", 28, "m", null, 1, 2, "kunder@kakao.com", 0, 1, "2020-01-20 06:28:47", "2020-01-20 06:28:47"));
-			resultMap.put("sender", tempObject(6, "SUNGCHAN", 25, "m", null, 2, 5, "tjdcksdl00@kakao.com", 0, 2, "2020-01-22 16:28:47", "2020-01-23 19:21:43"));
-			return response(resultMap, HttpStatus.OK, true);
-		} catch(RuntimeException e) {
-			return response(e.getMessage(), HttpStatus.CONFLICT, false);
-		}
-	}
-	
-	private Map<String, Object> tempObject(int uId, String nickname, int age, String gender, String image, int beforePsId, 
-			int afterPsId, String socialId, int provider, int level, String registerDate, String updateDate) {
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("uId", uId);
-		resultMap.put("nickname", nickname);
-		resultMap.put("age", age);
-		resultMap.put("gender", gender);
-		resultMap.put("image", image);
-		resultMap.put("beforePsId", beforePsId);
-		resultMap.put("afterPsId", afterPsId);
-		resultMap.put("socialId", socialId);
-		resultMap.put("provider", provider);
-		resultMap.put("level", level);
-		resultMap.put("registerDate", registerDate);
-		resultMap.put("updateDate", updateDate);
-		
-		return resultMap;
-	}
-	
 	@GetMapping("/community/selectAll")
 	public ResponseEntity<Map<String, Object>> selectAll() {
 		try {
