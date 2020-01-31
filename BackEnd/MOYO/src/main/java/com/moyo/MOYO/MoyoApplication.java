0b73c52d4f1f4ddcad3c29ae400ab635f57f9052
IndongLee <kundera@kakao.com>
2020-01-31 10:06:43 +0900

    update user profile update

--- BackEnd/MOYO/src/main/java/com/moyo/MOYO/MoyoApplication.java
+++ BackEnd/MOYO/src/main/java/com/moyo/MOYO/MoyoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
