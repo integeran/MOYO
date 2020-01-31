a147fea3aea10011e900d4731de54a30a3dc4f23
김성찬 <tjdcksdl00@naver.com>
2020-01-31 17:52:10 +0900

    update

--- FrontEnd/src/components/accompany/AccompanySearchBar.js
+++ FrontEnd/src/components/accompany/AccompanySearchBar.js
@@ -5,7 +5,6 @@ import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
-import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   root: {
