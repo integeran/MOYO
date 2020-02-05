c3714b35160969d9d8754947576baf32a8200989
Youngjune Jang <cyj9212@gmail.com>
2020-02-05 18:07:58 +0900

    modify accompany main page

--- FrontEnd/src/pages/acc/AccompanyMain.js
+++ FrontEnd/src/pages/acc/AccompanyMain.js
@@ -1,46 +1,96 @@
-import React from 'react';
-import styled from 'styled-components';
-import Button from '@material-ui/core/Button';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
+import axios from '../../api/axios';
+import moment from 'moment';
 
-const MainButtonDiv = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-content: center;
-`;
+import ButtonContents from '../../components/accompany/main/ButtonContents';
+import ScheduleContents from '../../components/accompany/main/ScheduleContents';
+import AdvertisingContents from '../../components/accompany/main/AdvertisingContents';
 
-const MainButton = styled(Button)`
-  padding: 3rem;
-  border: 1px solid black;
-`;
+import { Grid } from '@material-ui/core';
 
 const AccompanyMain = () => {
   const history = useHistory();
+  const userData = useSelector(state => state.auth.userData);
+  const [tripSchedule, setTripSchedule] = useState([]);
+  const [tripCompanion, setTripCompanion] = useState([]);
+
+  const getTripSchedule = async () => {
+    try {
+      const momentDate = moment(new Date()).format('YYYY-MM-DD');
+      return await axios.get(
+        `scheduleList/selectAllByUser/${userData.uid}/${momentDate}`,
+        {
+          headers: { userToken: userData.userToken },
+        },
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTripCompanion = async () => {
+    try {
+      const momentDate = moment(new Date()).format('YYYY-MM-DD');
+      return await axios.get(
+        `dailyAccompany/selectAllByUser/${userData.uid}/${momentDate}`,
+        {
+          headers: { userToken: userData.userToken },
+        },
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchTripInfo = async () => {
+      const sRes = await getTripSchedule();
+      const cRes = await getTripCompanion();
+      if (sRes && sRes.data && sRes.data.data) {
+        setTripSchedule(sRes.data.data);
+      }
+      if (cRes && cRes.data && cRes.data.data) {
+        setTripCompanion(cRes.data.data);
+      }
+    };
+
+    fetchTripInfo();
+  }, []);
+
   const handleAccompanyFindClick = () => {
     history.push({
       pathname: '/acc/accSetLoc',
       state: { prevpath: history.location.pathname },
     });
   };
+
   const handleAccompanyWriteClick = () => {
     history.push({
       pathname: '/acc/write',
       state: { prevpath: history.location.pathname },
     });
   };
+
   return (
-    <div>
-      <p>오늘은 여행이 어때요?</p>
-      <MainButtonDiv>
-        <MainButton variant="outlined" onClick={handleAccompanyFindClick}>
-          동행 구하기
-        </MainButton>
-        <MainButton variant="outlined" onClick={handleAccompanyWriteClick}>
-          동행 글 작성
-        </MainButton>
-      </MainButtonDiv>
-      <p>일정</p>
-    </div>
+    <Grid container direction="column" spacing={5}>
+      <Grid item>
+        <ButtonContents
+          onFindClick={handleAccompanyFindClick}
+          onWriteClick={handleAccompanyWriteClick}
+        />
+      </Grid>
+      <Grid item>
+        <ScheduleContents
+          tripSchedule={tripSchedule}
+          tripCompanion={tripCompanion}
+        />
+      </Grid>
+      <Grid item>
+        <AdvertisingContents />
+      </Grid>
+    </Grid>
   );
 };
 
