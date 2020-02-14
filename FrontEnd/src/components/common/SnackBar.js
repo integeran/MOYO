bbbda79ff1793419f3e6aea8bdf7ee723aa50637
integeran <jmiha22@gmail.com>
2020-02-14 17:56:55 +0900

    create dialog and snackbar update postmapchat

---
+++ FrontEnd/src/components/common/SnackBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Snackbar, IconButton, CloseIcon } from '@material-ui/core';
+
+const SnackBar = ({ contents }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={contents}
+        action={
+          <React.Fragment>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
+    </div>
+  );
+};
+export default SnackBar;
