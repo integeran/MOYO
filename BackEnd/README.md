2624c415b5e91a9e85fe3817415627ebc5d28397
integeran <jmiha22@gmail.com>
2020-02-14 13:10:57 +0900

    Apply .gitignore

--- BackEnd/README.md
+++
@@ -1,8 +0,0 @@
-﻿# DevTech
- ### Back
-   * SpringBoot
-
-## 개발자
-* 이인동
-* 김성찬
-* 정수안
--- FrontEnd/src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: process.env.REACT_APP_SUNGCHAN_ADDR,
-});
-
-export default API;
